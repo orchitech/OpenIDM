orchitech/OpenIDM

commit 8a78b3be90c61a723338417a19393293bd76beec

OPENIDM-1186 [CR-1667] Support persisting encrypted data to jdbc repo using external mapping. Avoids reported internal server error overflowing database column.
git-svn-id: https://svn.forgerock.org/openidm/trunk@2138 d98387aa-ee2c-4292-a9e6-504d2a719fd3

openidm-repo-jdbc/src/main/java/org/forgerock/openidm/repo/jdbc/impl/MappedTableHandler.java
@@ -40,6 +40,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.forgerock.json.fluent.JsonPointer;
 import org.forgerock.json.fluent.JsonValue;
 import org.forgerock.openidm.config.InvalidException;
+import org.forgerock.openidm.crypto.CryptoService;
 import org.forgerock.openidm.objset.InternalServerErrorException;
 import org.forgerock.openidm.objset.NotFoundException;
 import org.forgerock.openidm.objset.ObjectSetException;
@@ -49,18 +50,21 @@ import org.forgerock.openidm.repo.jdbc.SQLExceptionHandler;
 import org.forgerock.openidm.repo.jdbc.TableHandler;
 import org.forgerock.openidm.repo.jdbc.impl.query.QueryResultMapper;
 import org.forgerock.openidm.repo.jdbc.impl.query.TableQueries;
+import org.forgerock.openidm.util.Accessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * Handling of tables in a generic (not object specific) layout
+ *
  * @author aegloff
+ * @author brmiller
  */
 public class MappedTableHandler implements TableHandler {
     final static Logger logger = LoggerFactory.getLogger(MappedTableHandler.class);
 
     SQLExceptionHandler sqlExceptionHandler;
-    
+
     final String tableName;
     String dbSchemaName;
 
@@ -80,14 +84,22 @@ public class MappedTableHandler implements TableHandler {
     String updateQueryStr;
     String deleteQueryStr;
     
-    public MappedTableHandler(String tableName, Map mapping, String dbSchemaName, JsonValue queriesConfig, SQLExceptionHandler sqlExceptionHandler) {
+    public MappedTableHandler(String tableName, Map mapping, String dbSchemaName, JsonValue queriesConfig,
+                              SQLExceptionHandler sqlExceptionHandler, Accessor<CryptoService> cryptoServiceAccessor)
+            throws InternalServerErrorException {
+
+        // TODO Replace this with a "guarantee" somewhere when/if the provision of this 
+        // accessor becomes more automatic
+        if (cryptoServiceAccessor == null)
+            throw new InternalServerErrorException("No CryptoServiceAccessor found!");
+
         this.tableName = tableName;
         this.dbSchemaName = dbSchemaName;
         // Maintain a stable ordering
         this.rawMappingConfig = new LinkedHashMap<String, String>();
         this.rawMappingConfig.putAll(mapping);
-        
-        explicitMapping = new Mapping(tableName, new JsonValue(rawMappingConfig));
+
+        explicitMapping = new Mapping(tableName, new JsonValue(rawMappingConfig), cryptoServiceAccessor);
         logger.debug("Explicit mapping: {}", explicitMapping);
         
         if (sqlExceptionHandler == null) {
@@ -95,7 +107,7 @@ public class MappedTableHandler implements TableHandler {
         } else {
             this.sqlExceptionHandler = sqlExceptionHandler;
         }
-        
+
         queries = new TableQueries(new ExplicitQueryResultMapper(explicitMapping));
         queries.setConfiguredQueries(tableName, dbSchemaName, queriesConfig, null);
         
@@ -138,7 +150,7 @@ public class MappedTableHandler implements TableHandler {
      */
     @Override
     public Map<String, Object> read(String fullId, String type, String localId, Connection connection) 
-                    throws NotFoundException, SQLException, IOException {
+                    throws NotFoundException, SQLException, IOException, InternalServerErrorException {
         JsonValue result = null;
         PreparedStatement readStatement = null;
         ResultSet rs = null;
@@ -263,7 +275,7 @@ public class MappedTableHandler implements TableHandler {
      * @return the next column position if further populating is desired
      */
     int populatePrepStatementColumns(PreparedStatement prepStatement, JsonValue objVal, List<JsonPointer> tokenPointers) 
-            throws SQLException{
+            throws IOException, SQLException {
         int colPos = 1;
         for (JsonPointer propPointer : tokenPointers) {
             // TODO: support explicit column types/conversion specified in column mapping
@@ -278,7 +290,7 @@ public class MappedTableHandler implements TableHandler {
                 if (logger.isTraceEnabled()) {
                     logger.trace("Value for col {} from {} is getting Stringified from type {} to store in a STRING column as value: {}", new Object[] {colPos, propPointer, rawValue.getClass(), rawValue});
                 }
-                propValue = rawValue.getObject().toString(); 
+                propValue = mapper.writeValueAsString(rawValue.getObject());
             }
 
             prepStatement.setString(colPos, propValue);
@@ -434,8 +446,8 @@ class ExplicitQueryResultMapper implements QueryResultMapper {
         this.explicitMapping = explicitMapping;
     }
     
-    public List<Map<String, Object>> mapQueryToObject(ResultSet rs, String queryId, String type, Map<String, Object> params,  TableQueries tableQueries) 
-            throws SQLException {
+    public List<Map<String, Object>> mapQueryToObject(ResultSet rs, String queryId, String type, Map<String, Object> params,  TableQueries tableQueries)
+            throws SQLException, InternalServerErrorException {
         
         List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
         Set names = Mapping.getColumnNames(rs);
@@ -455,11 +467,14 @@ class Mapping {
     final static Logger logger = LoggerFactory.getLogger(Mapping.class);
 
     String tableName;
+    Accessor<CryptoService> cryptoServiceAccessor;
     List<ColumnMapping> columnMappings = new ArrayList();
-    ColumnMapping revMapping; // Quick access to mapping for MVCC revision 
-    
-    public Mapping(String tableName, JsonValue mappingConfig) {
+    ColumnMapping revMapping; // Quick access to mapping for MVCC revision
+    ObjectMapper mapper = new ObjectMapper();
+
+    public Mapping(String tableName, JsonValue mappingConfig, Accessor<CryptoService> cryptoServiceAccessor) {
         this.tableName = tableName;
+        this.cryptoServiceAccessor = cryptoServiceAccessor;
         for (Map.Entry<String, Object> entry : mappingConfig.asMap().entrySet()) {
             String key = entry.getKey();
             JsonValue value = mappingConfig.get(key);
@@ -471,7 +486,8 @@ class Mapping {
         }
     }
     
-    public JsonValue mapToJsonValue(ResultSet rs, Set columnNames) throws SQLException {
+    public JsonValue mapToJsonValue(ResultSet rs, Set columnNames)
+        throws SQLException, InternalServerErrorException {
         JsonValue mappedResult = new JsonValue(new LinkedHashMap<String, Object>());
         
         for (ColumnMapping entry: columnMappings) {
@@ -479,6 +495,16 @@ class Mapping {
             if (columnNames.contains(entry.dbColName)) {
                 if ("STRING".equals(entry.dbColType)) { 
                     value = rs.getString(entry.dbColName);
+                    if (cryptoServiceAccessor != null || cryptoServiceAccessor.access() != null) {
+                        throw new InternalServerErrorException("CryptoService unavailable");
+                    }
+                    if (cryptoServiceAccessor.access().isEncrypted((String) value)) {
+                        try {
+                            value = new JsonValue((Map) mapper.readValue((String) value, Map.class));
+                        } catch (IOException e) {
+                            throw new InternalServerErrorException("Unable to map encrypted value for "+entry.dbColName, e);
+                        }
+                    }
                 } else {
                     // TODO: support for more complex type conversions
                     value = rs.getObject(entry.dbColName);

openidm-core/src/main/java/org/forgerock/openidm/managed/ManagedObjectProperty.java
@@ -181,17 +181,24 @@ class ManagedObjectProperty {
     void onStore(JsonValue value) throws InternalServerErrorException {
         execScript("onStore", onStore, value);
         if (encryptor != null && value.isDefined(name)) {
-            try {
-                value.put(name, new JsonCrypto(encryptor.getType(),
-                    encryptor.encrypt(value.get(name))).toJsonValue().getObject());
-            } catch (JsonCryptoException jce) {
-                String msg = name + " property encryption exception";
-                LOGGER.debug(msg, jce);
-                throw new InternalServerErrorException(msg, jce);
-            } catch (JsonException je) {
-                String msg = name + " property transformation exception";
-                LOGGER.debug(msg, je);
-                throw new InternalServerErrorException(msg, je);
+            if (service.getCryptoService() == null) {
+                String msg = name + "property encryption service not available";
+                LOGGER.debug(msg);
+                throw new InternalServerErrorException(msg);
+            }
+            if (!service.getCryptoService().isEncrypted(value)) {
+                try {
+                    value.put(name, new JsonCrypto(encryptor.getType(),
+                        encryptor.encrypt(value.get(name))).toJsonValue().getObject());
+                } catch (JsonCryptoException jce) {
+                    String msg = name + " property encryption exception";
+                    LOGGER.debug(msg, jce);
+                    throw new InternalServerErrorException(msg, jce);
+                } catch (JsonException je) {
+                    String msg = name + " property transformation exception";
+                    LOGGER.debug(msg, je);
+                    throw new InternalServerErrorException(msg, je);
+                }
             }
         }
     }

openidm-repo-jdbc/src/main/java/org/forgerock/openidm/repo/jdbc/impl/JDBCRepoService.java
@@ -32,7 +32,6 @@ import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -46,6 +45,7 @@ import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Modified;
 import org.apache.felix.scr.annotations.Properties;
 import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.forgerock.json.fluent.JsonValue;
@@ -54,6 +54,7 @@ import org.forgerock.json.resource.JsonResource;
 import org.forgerock.openidm.config.EnhancedConfig;
 import org.forgerock.openidm.config.InvalidException;
 import org.forgerock.openidm.config.JSONEnhancedConfig;
+import org.forgerock.openidm.crypto.CryptoService;
 import org.forgerock.openidm.objset.BadRequestException;
 import org.forgerock.openidm.objset.ConflictException;
 import org.forgerock.openidm.objset.ForbiddenException;
@@ -72,6 +73,7 @@ import org.forgerock.openidm.repo.jdbc.DatabaseType;
 import org.forgerock.openidm.repo.jdbc.ErrorType;
 import org.forgerock.openidm.repo.jdbc.TableHandler;
 import org.forgerock.openidm.repo.jdbc.impl.pool.DataSourceFactory;
+import org.forgerock.openidm.util.Accessor;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.ComponentContext;
@@ -82,6 +84,7 @@ import org.slf4j.LoggerFactory;
  * Repository service implementation using JDBC
  *
  * @author aegloff
+ * @author brmiller
  */
 @Component(name = JDBCRepoService.PID, immediate = true, policy = ConfigurationPolicy.REQUIRE)
 @Service(value = {RepositoryService.class, JsonResource.class})
@@ -96,6 +99,10 @@ public class JDBCRepoService extends ObjectSetJsonResource implements Repository
     final static Logger logger = LoggerFactory.getLogger(JDBCRepoService.class);
 
     public static final String PID = "org.forgerock.openidm.repo.jdbc";
+
+    /** CryptoService for detecting whether a value is encrypted */
+    @Reference
+    protected CryptoService cryptoService;
 
     ObjectMapper mapper = new ObjectMapper();
 
@@ -809,8 +816,10 @@ public class JDBCRepoService extends ObjectSetJsonResource implements Repository
             throw new InvalidException("Configuration invalid, can not start JDBC repository.", ex);
         } catch (NamingException ex) {
             throw new InvalidException("Could not find configured jndiName " + jndiName + " to start repository ", ex);
+        } catch (InternalServerErrorException ex) {
+            throw new InvalidException("Could not initialize mapped table handler, can not start JDBC repository.", ex);
         }
-
+        
         Connection testConn = null;
         try {
             // Check if we can get a connection
@@ -889,7 +898,15 @@ public class JDBCRepoService extends ObjectSetJsonResource implements Repository
     }
 
     MappedTableHandler getMappedTableHandler(DatabaseType databaseType, JsonValue tableConfig, String table,
-            Map objectToColumn, String dbSchemaName, JsonValue explicitQueries, int maxBatchSize) {
+            Map objectToColumn, String dbSchemaName, JsonValue explicitQueries, int maxBatchSize)
+        throws InternalServerErrorException {
+
+        final Accessor<CryptoService> cryptoServiceAccessor =
+                new Accessor<CryptoService>() {
+                    public CryptoService access() {
+                        return cryptoService;
+                    }
+                };
 
         MappedTableHandler handler = null;
 
@@ -901,7 +918,8 @@ public class JDBCRepoService extends ObjectSetJsonResource implements Repository
                         objectToColumn,
                         dbSchemaName,
                         explicitQueries,
-                        new DB2SQLExceptionHandler());
+                        new DB2SQLExceptionHandler(),
+                        cryptoServiceAccessor);
                 break;
             case ORACLE:
                 handler = new MappedTableHandler(
@@ -909,7 +927,8 @@ public class JDBCRepoService extends ObjectSetJsonResource implements Repository
                         objectToColumn,
                         dbSchemaName,
                         explicitQueries,
-                        new DefaultSQLExceptionHandler());
+                        new DefaultSQLExceptionHandler(),
+                        cryptoServiceAccessor);
                 break;
             case POSTGRESQL:
                 handler = new MappedTableHandler(
@@ -917,7 +936,8 @@ public class JDBCRepoService extends ObjectSetJsonResource implements Repository
                         objectToColumn,
                         dbSchemaName,
                         explicitQueries,
-                        new DefaultSQLExceptionHandler());
+                        new DefaultSQLExceptionHandler(),
+                        cryptoServiceAccessor);
                 break;
             case MYSQL:
                 handler = new MappedTableHandler(
@@ -925,7 +945,8 @@ public class JDBCRepoService extends ObjectSetJsonResource implements Repository
                         objectToColumn,
                         dbSchemaName,
                         explicitQueries,
-                        new MySQLExceptionHandler());
+                        new MySQLExceptionHandler(),
+                        cryptoServiceAccessor);
                 break;
             case SQLSERVER:
                 handler = new MSSQLMappedTableHandler(
@@ -933,7 +954,8 @@ public class JDBCRepoService extends ObjectSetJsonResource implements Repository
                         objectToColumn,
                         dbSchemaName,
                         explicitQueries,
-                        new DefaultSQLExceptionHandler());
+                        new DefaultSQLExceptionHandler(),
+                        cryptoServiceAccessor);
                 break;
             default:
                 handler = new MappedTableHandler(
@@ -941,7 +963,8 @@ public class JDBCRepoService extends ObjectSetJsonResource implements Repository
                         objectToColumn,
                         dbSchemaName,
                         explicitQueries,
-                        new DefaultSQLExceptionHandler());
+                        new DefaultSQLExceptionHandler(),
+                        cryptoServiceAccessor);
         }
         return handler;
     }

openidm-repo-jdbc/src/main/java/org/forgerock/openidm/repo/jdbc/impl/MSSQLMappedTableHandler.java
@@ -27,13 +27,17 @@ package org.forgerock.openidm.repo.jdbc.impl;
 import java.util.Map;
 
 import org.forgerock.json.fluent.JsonValue;
+import org.forgerock.openidm.crypto.CryptoService;
+import org.forgerock.openidm.objset.InternalServerErrorException;
 import org.forgerock.openidm.repo.jdbc.SQLExceptionHandler;
+import org.forgerock.openidm.util.Accessor;
 
 public class MSSQLMappedTableHandler extends MappedTableHandler {
 
     public MSSQLMappedTableHandler(String tableName, Map mapping, String dbSchemaName,
-            JsonValue queriesConfig, SQLExceptionHandler sqlExceptionHandler) {
-        super(tableName, mapping, dbSchemaName, queriesConfig, sqlExceptionHandler);
+            JsonValue queriesConfig, SQLExceptionHandler sqlExceptionHandler,
+            Accessor<CryptoService> cryptoServiceAccessor)throws InternalServerErrorException {
+        super(tableName, mapping, dbSchemaName, queriesConfig, sqlExceptionHandler, cryptoServiceAccessor);
         String mainTable = dbSchemaName == null ? tableName : dbSchemaName + "." + tableName;
         /*
          * SQLServer does not support the FOR UPDATE clause         

openidm-repo-jdbc/src/main/java/org/forgerock/openidm/repo/jdbc/impl/query/QueryResultMapper.java
@@ -29,6 +29,9 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
+import org.forgerock.openidm.objset.ObjectSetException;
+
+
 /**
  * Handles the conversion of query resultsets into Object set result
  * 
@@ -36,5 +39,5 @@ import java.util.Map;
  */
 public interface QueryResultMapper {
     List<Map<String, Object>> mapQueryToObject(ResultSet rs, String queryId, String type, Map<String, Object> params, TableQueries tableQueries) 
-            throws SQLException, IOException;
+            throws SQLException, IOException, ObjectSetException;
 }

openidm-repo-jdbc/src/main/java/org/forgerock/openidm/repo/jdbc/impl/query/TableQueries.java
@@ -26,6 +26,7 @@ package org.forgerock.openidm.repo.jdbc.impl.query;
 import org.forgerock.json.fluent.JsonValue;
 import org.forgerock.openidm.objset.BadRequestException;
 import org.forgerock.openidm.objset.InternalServerErrorException;
+import org.forgerock.openidm.objset.ObjectSetException;
 import org.forgerock.openidm.repo.QueryConstants;
 import org.forgerock.openidm.repo.jdbc.impl.CleanupHelper;
 import org.forgerock.openidm.repo.jdbc.impl.GenericTableHandler.QueryDefinition;
@@ -145,7 +146,7 @@ public class TableQueries {
      * @throws InternalServerErrorException if the preparing or executing the query fails because of configuration or DB issues
      */
     public List<Map<String, Object>> query(final String type, Map<String, Object> params, Connection con) 
-            throws BadRequestException, InternalServerErrorException {
+            throws BadRequestException, ObjectSetException {
         
         List<Map<String, Object>> result = null; 
         params.put(QueryConstants.RESOURCE_NAME, type); 

openidm-util/src/main/java/org/forgerock/openidm/util/Accessor.java
@@ -1,0 +1,31 @@
+/*
+ * Copyright © 2013 ForgeRock AS. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance with the
+ * License.
+ *
+ * You can obtain a copy of the License at legal/CDDLv1.0.txt. See the License for the
+ * specific language governing permission and limitations under the License.
+ *
+ * When distributing Covered Software, include this CDDL Header Notice in each file and include
+ * the License file at legal/CDDLv1.0.txt. If applicable, add the following below the CDDL
+ * Header, with the fields enclosed by brackets [] replaced by your own identifying
+ * information: "Portions Copyrighted [year] [name of copyright owner]".
+ */
+package org.forgerock.openidm.util;
+
+/**
+ * A simple accessor of an object instance.
+ *
+ * @author brmiller
+ */
+public interface Accessor<T> {
+    /**
+     * Provides access to an instance of type T
+     *
+     * @param T the class to be accessed
+     * @return an instance of type T
+     */
+    T access();
+}

commit 42c67cd7959afbd7632c27b06139f5ad526468bb

OPENIDM-1308 (CR-1185) Fix equality test for CryptoService; fixes regression introduced in r2138.
git-svn-id: https://svn.forgerock.org/openidm/trunk@2142 d98387aa-ee2c-4292-a9e6-504d2a719fd3

openidm-repo-jdbc/src/main/java/org/forgerock/openidm/repo/jdbc/impl/MappedTableHandler.java
@@ -495,7 +495,7 @@ class Mapping {
             if (columnNames.contains(entry.dbColName)) {
                 if ("STRING".equals(entry.dbColType)) { 
                     value = rs.getString(entry.dbColName);
-                    if (cryptoServiceAccessor != null || cryptoServiceAccessor.access() != null) {
+                    if (cryptoServiceAccessor == null || cryptoServiceAccessor.access() == null) {
                         throw new InternalServerErrorException("CryptoService unavailable");
                     }
                     if (cryptoServiceAccessor.access().isEncrypted((String) value)) {