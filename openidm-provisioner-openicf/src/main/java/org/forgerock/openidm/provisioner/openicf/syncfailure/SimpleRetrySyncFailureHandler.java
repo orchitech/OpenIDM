orchitech/OpenIDM

commit 46f0d9f6e8c7c2db824ad91e76d7dbffc8da24e1

OPENIDM-1326 (CR-2019) SyncToken value is not always an integer--don't make type assumption.
git-svn-id: https://svn.forgerock.org/openidm/trunk@2296 d98387aa-ee2c-4292-a9e6-504d2a719fd3

openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/syncfailure/SimpleRetrySyncFailureHandler.java
@@ -37,7 +37,7 @@ public class SimpleRetrySyncFailureHandler implements SyncFailureHandler {
     private final SyncFailureHandler postRetryHandler;
 
     /** current token being retried */
-    private int currentSyncToken;
+    private Object currentSyncToken;
 
     /** number of retries on current token */
     private int currentRetries;
@@ -63,7 +63,7 @@ public class SimpleRetrySyncFailureHandler implements SyncFailureHandler {
     public void invoke(JsonValue syncFailure, Exception failureCause)
         throws SyncHandlerException {
 
-        final int token = syncFailure.get("token").asInteger().intValue();
+        final Object token = syncFailure.get("token");
 
         if (token == currentSyncToken) {
             currentRetries++;

openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/syncfailure/DeadLetterQueueHandler.java
@@ -59,7 +59,7 @@ public class DeadLetterQueueHandler implements SyncFailureHandler {
         String id = new StringBuffer("repo/synchronisation/deadLetterQueue/")
             .append(syncFailure.get("systemIdentifier").asString())
             .append("/")
-            .append(syncFailure.get("token").asInteger())
+            .append(syncFailure.get("token").toString())
             .toString();
 
         try {

openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/syncfailure/LoggedIgnoreHandler.java
@@ -39,7 +39,7 @@ public class LoggedIgnoreHandler implements SyncFailureHandler {
         throws SyncHandlerException {
         logger.warn("{} liveSync failure on sync-token {} for {}, {} - {}",
                 syncFailure.get("systemIdentifier").asString(),
-                syncFailure.get("token").asInteger(),
+                syncFailure.get("token").toString(),
                 syncFailure.get("objectType").asString(),
                 syncFailure.get("uid").asString(),
                 failureCause.toString());

commit c943617be88aeb9abe7029236957958816856a93

OPENIDM-1326 (skype review by Nicolas) Regression in r2296 when changing sync token from int to Object; must use .equals
git-svn-id: https://svn.forgerock.org/openidm/trunk@2330 d98387aa-ee2c-4292-a9e6-504d2a719fd3

openidm-provisioner-openicf/src/main/java/org/forgerock/openidm/provisioner/openicf/syncfailure/SimpleRetrySyncFailureHandler.java
@@ -66,7 +66,7 @@ public class SimpleRetrySyncFailureHandler implements SyncFailureHandler {
 
         final Object token = syncFailure.get("token");
 
-        if (token == currentSyncToken) {
+        if (token != null && token.equals(currentSyncToken)) {
             currentRetries++;
         }
         else {