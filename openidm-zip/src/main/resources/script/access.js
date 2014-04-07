orchitech/OpenIDM

commit 7d31899a3fbcfefea94877b1dbab65066b4dc944

Tightening the default authz rule for non-admin users - action and query are not necessary. Reviewed with Andi via Skype
git-svn-id: https://svn.forgerock.org/openidm/trunk@3030 d98387aa-ee2c-4292-a9e6-504d2a719fd3

openidm-zip/src/main/resources/script/access.js
@@ -153,10 +153,14 @@ var httpAccessConfig =
             "methods"   : "action",
             "actions"   : "reauthenticate"
         },
+
+        // This rule is primarily controlled by the ownDataOnly function - that will only allow 
+        // access to the endpoint from which the user orignates 
+        // (For example a managed/user with the _id of bob will only be able to access managed/user/bob)
         {   
             "pattern"   : "*",
             "roles"     : "openidm-authorized",
-            "methods"   : "read,update,patch,action,query", // note the missing 'delete' - by default, users cannot delete themselves
+            "methods"   : "read,update,patch",
             "actions"   : "*",
             "customAuthz" : "ownDataOnly() && managedUserRestrictedToAllowedProperties('"+allowedPropertiesForManagedUser+"') && disallowQueryExpression()"
         },

commit 078ce69166c6a3ef095a11b1145c3f7791aabad9

Fixing bug introduced in r3030 related to patch requests made with ?_action=patch - OPENIDM-1766
Reviewed in Skype

git-svn-id: https://svn.forgerock.org/openidm/trunk@3056 d98387aa-ee2c-4292-a9e6-504d2a719fd3

openidm-zip/src/main/resources/script/access.js
@@ -160,8 +160,8 @@ var httpAccessConfig =
         {   
             "pattern"   : "*",
             "roles"     : "openidm-authorized",
-            "methods"   : "read,update,patch",
-            "actions"   : "*",
+            "methods"   : "read,update,patch,action",
+            "actions"   : "patch",
             "customAuthz" : "ownDataOnly() && managedUserRestrictedToAllowedProperties('"+allowedPropertiesForManagedUser+"') && disallowQueryExpression()"
         },
 